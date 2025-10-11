4a21ccbf6962090f0153f9040110fd2520ea38d3
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-10-11 19:40:23 +0800

    Update globaldefs.h

--- globaldefs.h
+++ globaldefs.h
@@ -7,14 +7,16 @@
 
 #include <chrono>
 #include <string>
+#include <random>
 
-const int                       g_unitX         = 20;
-const int                       g_unitY         = 20;
-const int                       g_initialLength = 3;
-const sf::Color                 g_bodyColor     = sf::Color::Green;
-const sf::Color                 g_foodColor     = sf::Color::Red;
-const std::chrono::milliseconds g_movingInterval(80);
-const std::string               g_fontFile("Zxunge Hand-Regular.ttf");
+const int                              g_unitX         = 20;
+const int                              g_unitY         = 20;
+const int                              g_initialLength = 3;
+inline const sf::Color                 g_bodyColor     = sf::Color::Green;
+inline const sf::Color                 g_foodColor     = sf::Color::Red;
+inline const std::chrono::milliseconds g_movingInterval(80);
+inline const std::string               g_fontFile("Zxunge Hand-Regular.ttf");
+extern const std::mt19937              g_randGen;
 
 enum class Direction
 {
