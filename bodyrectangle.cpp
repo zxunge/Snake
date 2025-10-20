112886f9c571a4dc1aa27320a9f2db24c37ca339
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2025-10-20 11:50:47 +0800

    Refactor BodyRectangle constructor to use initializer lists.

--- bodyrectangle.cpp
+++ bodyrectangle.cpp
@@ -7,13 +7,13 @@ using namespace sf;
 BodyRectangle::BodyRectangle()
 {
     setFillColor(g_bodyColor);
-    setSize(Vector2f(g_unitX, g_unitY));
-    setPosition(0.0, 0.0);
+    setSize({g_unitX, g_unitY});
+    setPosition({0, 0});
 }
 
 BodyRectangle::BodyRectangle(sf::Vector2f pos)
 {
     setFillColor(g_bodyColor);
-    setSize(Vector2f(g_unitX, g_unitY));
-    setPosition(pos.x * g_unitX, pos.y * g_unitY);
+    setSize({g_unitX, g_unitY});
+    setPosition({pos.x * g_unitX, pos.y * g_unitY});
 }
